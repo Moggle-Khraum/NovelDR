19fde15f1e52525fccb854e84c370212684aea13
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-06-18 07:07:41 +0800

    Update app.config.js

--- artifacts/novel-reader/app.config.js
+++ artifacts/novel-reader/app.config.js
@@ -6,7 +6,7 @@ export default () => {
     expo: {
       name: "Novel DR",
       slug: "novel-reader",
-      version: "2.15.10",
+      version: "2.18.12",
       owner: "moggstones-stash",   // 🔧 Add this line
       orientation: "portrait",
       icon: "./assets/images/icon.png",
