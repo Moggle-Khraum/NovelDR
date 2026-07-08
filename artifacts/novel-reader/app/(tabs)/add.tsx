e6a2bf375a68c00094f153da6dafcb63ac7e63f1
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-07-08 12:29:33 +0800

    Update add.tsx

--- artifacts/novel-reader/app/(tabs)/add.tsx
+++ artifacts/novel-reader/app/(tabs)/add.tsx
@@ -32,12 +32,12 @@ const SUPPORTED_SITES = [
   { name: "AllNovelOrg", baseUrl: "https://allnovel.org/" },
   { name: "FreeWebNovelCom", baseUrl: "https://freewebnovel.com/" },
   { name: "NovGoNet", baseUrl: "https://novgo.net/" },
-  { name: "NovelBinCom", baseUrl: "https://novelbin.com/" },
-  { name: "BedNovelCom", baseUrl: "https://bednovel.com/" },
   { name: "LightNovelWorldOrg", baseUrl: "https://lightnovelworld.org/" },
   { name: "WuxiaWorldSite", baseUrl: "https://wuxiaworld.site/" },
   { name: "RoyalRoad", baseUrl: "https://royalroad.com/" },
   { name: "AsiaNovel", baseUrl: "https://asianovel.net/" },
+  { name: "NovelPhoenix", baseUrl: "https://novelphoenix.com/" },
+  
 ];
 
 type LogEntry = {
