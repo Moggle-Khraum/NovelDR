b20182e0fbc0b712f5792b41a94ca1816999fdfa
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-07-16 13:33:54 +0800

    Add files via upload

--- artifacts/novel-reader/app/_layout.tsx
+++ artifacts/novel-reader/app/_layout.tsx
@@ -24,6 +24,7 @@ import { ErrorBoundary } from "@/components/ErrorBoundary";
 import { ThemeProvider, useTheme } from "@/context/ThemeContext";
 import { LibraryProvider, useLibrary } from "@/context/LibraryContext";
 import { UpdateProvider } from "@/context/UpdateContext";
+import { WebViewFetchBridge } from "@/hooks/scrapers/shared/webviewBridge";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -313,6 +314,7 @@ export default function RootLayout() {
               <LibraryProvider>
                 <UpdateProvider>
                   <RootLayoutNav />
+                  <WebViewFetchBridge />
                 </UpdateProvider>
               </LibraryProvider>
             </ThemeProvider>
