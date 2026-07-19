5239ce3aabe77ae8c8269a6bed5a3082176be88f
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-07-19 21:13:45 +0800

    Update ErrorBoundary.tsx

--- artifacts/novel-reader/components/ErrorBoundary.tsx
+++ artifacts/novel-reader/components/ErrorBoundary.tsx
@@ -1,6 +1,6 @@
-import React, { Component, ComponentType, PropsWithChildren } from "react";
+import React, { Component, ComponentType, PropsWithChildren } from 'react';
 
-import { ErrorFallback, ErrorFallbackProps } from "@/components/ErrorFallback";
+import { ErrorFallback, ErrorFallbackProps } from '@/components/ErrorFallback';
 
 export type ErrorBoundaryProps = PropsWithChildren<{
   FallbackComponent?: ComponentType<ErrorFallbackProps>;
@@ -13,10 +13,7 @@ type ErrorBoundaryState = { error: Error | null };
  * This is a special case for for using the class components. Error boundaries must be class components because React only provides error boundary functionality through lifecycle methods (componentDidCatch and getDerivedStateFromError) which are not available in functional components.
  * https://react.dev/reference/react/Component#catching-rendering-errors-with-an-error-boundary
  */
-export class ErrorBoundary extends Component<
-  ErrorBoundaryProps,
-  ErrorBoundaryState
-> {
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   state: ErrorBoundaryState = { error: null };
 
   static defaultProps: {
@@ -30,7 +27,7 @@ export class ErrorBoundary extends Component<
   }
 
   componentDidCatch(error: Error, info: { componentStack: string }): void {
-    if (typeof this.props.onError === "function") {
+    if (typeof this.props.onError === 'function') {
       this.props.onError(error, info.componentStack);
     }
   }
@@ -43,12 +40,10 @@ export class ErrorBoundary extends Component<
     const { FallbackComponent } = this.props;
 
     return this.state.error && FallbackComponent ? (
-      <FallbackComponent
-        error={this.state.error}
-        resetError={this.resetError}
-      />
+      <FallbackComponent error={this.state.error} resetError={this.resetError} />
     ) : (
       this.props.children
     );
   }
 }
+
