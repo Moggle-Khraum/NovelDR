ea0b526756d1a5423c766a6882d34660901e58af
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-07-20 17:27:24 +0800

    Update app.config.js

--- artifacts/novel-reader/app.config.js
+++ artifacts/novel-reader/app.config.js
@@ -25,7 +25,7 @@ export default () => {
       android: {
         package: "com.noveldr.app",
         versionCode: parseInt(buildNumber, 10),
-        permissions: ["REQUEST_INSTALL_PACKAGES"],
+        permissions: ["REQUEST_INSTALL_PACKAGES"], "POST_NOTIFICATIONS",  // Required for Android 13+
       },
       web: {
         favicon: "./assets/images/icon.png",
