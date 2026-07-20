8587920731baf1cc3846620160db4ce4db877925
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-07-20 18:37:04 +0800

    Add files via upload

--- artifacts/novel-reader/app.config.js
+++ artifacts/novel-reader/app.config.js
@@ -25,7 +25,10 @@ export default () => {
       android: {
         package: "com.noveldr.app",
         versionCode: parseInt(buildNumber, 10),
-        permissions: ["REQUEST_INSTALL_PACKAGES", "POST_NOTIFICATIONS"],
+        permissions: [
+          "REQUEST_INSTALL_PACKAGES",
+          "POST_NOTIFICATIONS",
+        ],
       },
       web: {
         favicon: "./assets/images/icon.png",
@@ -46,6 +49,14 @@ export default () => {
             color: "#FFFFFF",
             sounds: [],
             modes: "production",
+          },
+        ],
+        [
+          "expo-build-properties",
+          {
+            android: {
+              compileSdkVersion: 34,
+            },
           },
         ],
         [
