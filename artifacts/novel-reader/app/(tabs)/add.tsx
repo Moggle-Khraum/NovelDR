394f1c03bd30e543988cf2f10c13b40f38bda611
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-07-20 10:32:45 +0800

    Update add.tsx

--- artifacts/novel-reader/app/(tabs)/add.tsx
+++ artifacts/novel-reader/app/(tabs)/add.tsx
@@ -41,6 +41,7 @@ const SUPPORTED_SITES = [
   { name: "RoyalRoad", baseUrl: "https://royalroad.com/" },
   { name: "AsiaNovel", baseUrl: "https://asianovel.net/" },
   { name: "NovelPhoenix", baseUrl: "https://novelphoenix.com/" },
+  { name: "NovelArrow", baseUrl: "https://novelarrow.com/"},
   { name: "Novel-Bin", baseUrl: "https://novel-bin.com/" },
   { name: "NovelBinCC", baseUrl: "https://www.novelbin.cc/" },
 ];
