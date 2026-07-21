0414bc9bbbdf4759aa6e0bcf4af4036d6a5d741d
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-07-21 15:07:39 +0800

    Add files via upload

--- artifacts/novel-reader/app/novel/[id].tsx
+++ artifacts/novel-reader/app/novel/[id].tsx
@@ -1,7 +1,7 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import * as Haptics from "expo-haptics";
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 import * as Sharing from "expo-sharing";
 import * as Print from "expo-print";
 import { router, useLocalSearchParams } from "expo-router";
